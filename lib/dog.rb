ca79804c6be4d8ef7abc945382566b11cd30075c
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-23 14:58:11 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    #writes name
+    def name=(name)
+        @name = name
+    end
+    #reads name
+    def name
+        @name
+    end
+
+    #writes breed
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #reads breed
+    def breed
+        @breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    #writes name
+    def name=(name)
+        @name = name
+    end
+    #reads name
+    def name
+        @name
+    end
+    #write job
+    def job=(job)
+        @job = job
+    end
+    #reads job
+    def job
+        @job
+    end
+end
